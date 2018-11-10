c737d123b51340b782780b8e580828e3dd97bf83
Ryan Davis <rdavis54@uwo.ca>
2018-11-10 17:44:17 -0500

    changes mades

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost = "localhost";
 $dbuser= "root";
-$dbpass = "cs3319";
-$dbname = "vetoffice";
+$dbpass = "";
+$dbname = "rdavis54assign2db";
 $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
 if (mysqli_connect_errno()) {
      die("database connection failed :" .
