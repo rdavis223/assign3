ae7fedafceb3011a0a5d69afcdc22bd19fddf27b
Ryan Davis <rdavis54@uwo.ca>
2018-11-11 14:41:17 -0500

    hey

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function displayCustomerData(){
 		echo "<h1>Customer has purchased the following products: </h1>";
 		$first = 0;
 		while ($row =mysqli_fetch_assoc($result)) {
-			if ($row = null){
+			if ($row == null){
 				echo "reached";
 			}
 			if ($first == 0 && $row == null){
