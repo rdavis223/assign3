1fec82c0cd658eb3617dde1c79485157305f6199
Ryan Davis <rdavis54@uwo.ca>
2018-11-12 16:30:00 -0500

    t

--- quantitySearch.php
+++ quantitySearch.php
@@ -11,15 +11,14 @@
   <tr>
     <td width="188" class="sidebar">Menu items will go here</td>
     <td width="796">
-    <form action="quantitySearch" method="post">
+    <form action="quantitySearch.php" method="post">
 	quantity: <input type="text" name="quantity"><br>
 	<input type="submit">
 	</form>
 	<?php
-function deleteCustomer(){
+function quantitySearch(){
 	include 'connectdb.php';
 	if (isset($_POST["quantity"])){
-		echo $_POST["customerID"];
 		$query = "SELECT customer.firstName, customer.lastName, purchase.quantity, product.description FROM (customer INNER JOIN purchase ON customer.customerID = purchase.customerID) INNER JOIN product ON purchase.productID = product.productID WHERE purchase.quantity > ".$_POST["quantity"] ;
 		$result = mysqli_query($connection, $query);
 		if (!$result){
@@ -41,13 +40,10 @@ function deleteCustomer(){
 		
 	}
 }
-deleteCustomer();
+quantitySearch();
 
 ?>
-<form action="deleteCustomer.php" method="post">
-customerID: <input type="text" name="customerID"><br>
-<input type="submit">
-</form>
+
 
 </td>
   </tr>
