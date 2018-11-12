4eec2b7034982b83a03595c4d28fef65c7932430
Ryan Davis <rdavis54@uwo.ca>
2018-11-12 15:57:15 -0500

    k

--- deleteCustomer.php
+++ deleteCustomer.php
@@ -15,7 +15,7 @@
 function deleteCustomer(){
 	include 'connectdb.php';
 	if (isset($_POST["customerID"])){
-		$query = "DELETE FROM customer WHERE customerID = '".$_POST["customerID"];
+		$query = "DELETE FROM customer WHERE customerID = '".$_POST["customerID"]."'";
 		$result = mysqli_query($connection,$query);
 		if (!$result){
 			echo mysqli_error($connection);
