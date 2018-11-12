ab15029f8b5fb7288e441772beac14619e14743c
Ryan Davis <rdavis54@uwo.ca>
2018-11-12 17:04:16 -0500

    t

---
+++ neverPurchased.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>First Item</title>
+<link href="assign3.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<table width="1000" border="1">
+  <tr>
+    <td width="188" class="sidebar">Menu items will go here</td>
+    <td width="796">
+	<?php
+function neverPurchased(){
+	include 'connectdb.php';
+	$query = "SELECT description FROM product WHERE productID NOT IN (SELECT productID FROM purchase)";
+	$result = mysqli_query($connection, $query);
+	if (!$result){
+		echo mysqli_error($connection);
+	} else {
+			$flag = 0;
+			while ($row =mysqli_fetch_assoc($result)) {
+				echo "<li>";
+				echo implode("",$row);
+				echo "</li>";
+				$flag = 1;
+			
+		}
+			if ($flag == 0){
+				echo "No products to show";
+		}
+	}
+}
+neverPurchased();
+
+?>
+
+
+</td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ totalSales.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>First Item</title>
+<link href="assign3.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<table width="1000" border="1">
+  <tr>
+    <td width="188" class="sidebar">Menu items will go here</td>
+    <td width="796">
+    <form action="totalSales.php" method="post">
+	productID: <input type="text" name="productID"><br>
+	<input type="submit">
+	</form>
+	<?php
+function neverPurchased(){
+	include 'connectdb.php';
+	$query = "SELECT description, purchase.quantity, cost FROM purchase INNER JOIN product ON product.productID = purchase.productID WHERE productID = '".$_POST["productID"]."'";
+	$result = mysqli_query($connection, $query);
+	if (!$result){
+		echo mysqli_error($connection);
+	} else {
+			$totalCost = 0;
+			$totalUnits = 0;
+			$desc = "";
+			$row = mysqli_fetch_assoc($result);
+			$totalCost = $totalCost + $row["cost"] *$row["quantity"];
+			$totalUnits = $totalUnits + $row["quantity"];
+			$desc = $row["description"];
+			while ($row =mysqli_fetch_assoc($result)) {
+				$totalCost = $totalCost + $row["cost"] * $row["quantity"];
+				$totalUnits = $totalUnits + $row["quantity"];
+		}
+		echo "<li>Product: ".$desc."</li>";
+		echo "<li>Units: ".$totalUnits."</li>";
+		echo "<li>Sales: $".$totalCost."</li>";
+			
+	}
+}
+neverPurchased();
+
+?>
+
+
+</td>
+  </tr>
+</table>
+</body>
+</html>
